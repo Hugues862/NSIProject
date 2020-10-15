46fd1a80c9c9bbd43196222c585e799ebbc314f3
Hugues862 <53710157+Hugues862@users.noreply.github.com>
2020-10-15 14:46:53 +0200

    Add files via upload

---
+++ HP/EventsFile.py
@@ -0,0 +1,13 @@
+def GameQuit():
+    return False
+
+def CheckKeyPress():
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+    
---
+++ HP/main.py
@@ -0,0 +1,48 @@
+import pygame
+import EventsFile
+pygame.init()
+
+win = pygame.display.set_mode((500,500))
+
+
+x = 50
+y = 50
+width = 50
+height = 50
+vel = 5
+
+#Event Listener
+run = True
+while run:
+    pygame.time.delay(30) # Clock for the game in ms
+
+    keys = pygame.key.get_pressed() #stores in keys all keys that are pressed
+
+    """Key Presses"""
+    EventsFile.Get
+
+
+    """Listening to the events """
+    for event in pygame.event.get(): #Get all events
+        
+        """Current Event list"""
+        if event.type == pygame.QUIT: #QUIT event
+            run = EventsFile.GameQuit() #returns False
+
+    
+
+    
+
+
+
+    win.fill((0,0,0))
+    pygame.draw.rect(win, (255, 0 , 0), (x, y, width, height))
+    
+    
+    pygame.display.update() #update game display, essential to refresh every frame
+
+pygame.quit()
+
+
+
+
