3c838e3038ce069d3d56f2d232b911a5ebd79e4a
JackyYC <yoann.chhang@gmail.com>
2020-10-22 09:50:53 +0200

    Platforms

---
+++ HP/stages.py
@@ -0,0 +1,9 @@
+import pygame
+import classes.platform as platform
+
+def initStages(window): # Don't forget 
+
+    Stage0 = [
+        platform.hardPlatform(window, 100, 600, 600, 50))
+    ]
+    
